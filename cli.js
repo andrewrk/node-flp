0a10e86d89642ddaafeb0ce2e72f39b9c5ec2310
Andrew Kelley <superjoe30@gmail.com>
2014-04-28 16:02:55 -0700

    add cli tool

---
+++ cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var flp = require('./');
+var inputFile = process.argv[2];
+
+if (!inputFile) {
+  console.log("Usage: flp-parse filee.flp");
+  process.exit(1);
+}
+
+flp.parseFile(inputFile, function(err, projectInfo) {
+  if (err) throw err;
+  console.log(projectInfo);
+});
